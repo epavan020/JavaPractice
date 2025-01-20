c50548fbf8fa045b358d77d7097f883056c50e80
PavanNaikEslavath <epavan020@gmail.com>
2025-01-20 13:28:54 +0530

    Loop changes

--- src/ForLoop1.java
+++ src/ForLoop1.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class ForLoop1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        for (i=1;i<=5;i++){
+            System.out.println(i);
+        }
+    }
 }
--- src/Pattern.java
+++ src/Pattern.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Pattern {
+    public static void main(String[] args) {
+        int number=1;
+        for (int i=1;i<=3;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+
+    }
 }
