1c5da79a9f6b76d64659872052f6540abda474f5
PavanNaikEslavath <epavan020@gmail.com>
2025-01-21 13:17:43 +0530

    Initial Commit

--- src/ForLoop1.java
+++ src/ForLoop1.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class ForLoop1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int i=sc.nextInt();
-        for (i=1;i<=5;i++){
-            System.out.println(i);
+        int number=12345;
+        String p=Integer.toString(number);
+        for (int i=p.length()-1;i>=0;i--){
+            System.out.println(p.charAt(i));
         }
     }
 }
